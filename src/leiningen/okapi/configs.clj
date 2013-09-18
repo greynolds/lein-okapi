7b3a359eef8192e85794d59c5f00e127735812a0
Gregg Reynolds <dev@mobileink.com>
2013-09-18 12:15:51 -0500

    simplify configs code

--- src/leiningen/okapi/configs.clj
+++ src/leiningen/okapi/configs.clj
@@ -11,42 +11,22 @@
             [clojure.string :as string]
             [clojure.tools.logging :as log :only [debug info]]))
 
-
-(defn setup-plugins
-  [config-mgr]
-  (let [plugins-mgr (PluginsManager.)]
-;        f ... dropins?]
-    (do
-      (.discover plugins-mgr  (io/file "./dropins") true)
-      (.addFromPlugins config-mgr plugins-mgr)
-      )))
-
-(defn setup
-  [config-mgr]
-  (let [filter-map {  ;; what's the purpose of this?
-                    ".docx" "okf_openxml"
-                    ".pptx" "okf_openxml"
-                    ".xlsx" "okf_openxml"
-                    ;; ...
-                    }]
+(defn configs [& args]
+  (let [config-mgr (FilterConfigurationMapper.)
+        plugins-mgr (PluginsManager.)]
     (do
       (DefaultFilters/setMappings config-mgr false true)
-      (setup-plugins config-mgr)))
-  )
-
-(defn configs [& args]
-  (let [config-mgr (FilterConfigurationMapper.)]
-    (setup config-mgr)
-    (.updateCustomConfigurations config-mgr)
-    (println "Listing available filter configurations")
-    (let [configs (.getAllConfigurations config-mgr)]
-      (do ;; (println "configs:" (type configs))
-      (doseq [config (sort-by #(.name %) (iterator-seq configs))]
-             (println (format "%s: %s" ;; = %s"
-                              (.name config)
-                              (.configId config)
-                              ;;(.description config)
-                              )))))
-    ))
+      (.discover plugins-mgr  (io/file "./dropins") true)
+      (.addFromPlugins config-mgr plugins-mgr)
+      (.updateCustomConfigurations config-mgr)
+      (println "Listing available filter configurations")
+      (let [configs (.getAllConfigurations config-mgr)]
+        (doseq [config (sort-by #(.name %) (iterator-seq configs))]
+          (println (format "%s: %s" ;; = %s"
+                           (.name config)
+                           (.configId config)
+                           ;;(.description config)
+                           )))))
+  ))
 
 
